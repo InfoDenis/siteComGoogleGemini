b5344352b759c442eb4fc46f8810c2a55d3e20b6
InfoDenis <mensagemparavoce_54@hotmail.com>
2024-09-07 15:21:45 -0300

    Aula04 - Construindo uma função para apresentar dos dados na tela após clicar no botão 'Pesquisar'.

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log(dados);
+// Função para realizar uma pesquisa e exibir os resultados na página.
+function pesquisar() {
+  // Obtém a seção onde os resultados serão exibidos.
+  let section = document.getElementById("resultados-pesquisa");
+  let resultados = ""; // String que armazenará os resultados formatados em HTML.
+
+  // Itera sobre cada dado da pesquisa e constrói o HTML para cada resultado.
+  for (let dado of dados) {
+    resultados += `
+      <div class="item-resultado">
+        <h2> <a href="#" target="_blank">${dado.titulo}</a></h2>
+        <p class="descricao-meta">${dado.descricao}</p>
+        <a href="${dado.link}" target="_blank">Mais Informações</a>
+      </div>
+    `;
+  }
+
+  // Atribui o HTML gerado à seção de resultados.
+  section.innerHTML = resultados;
+}
