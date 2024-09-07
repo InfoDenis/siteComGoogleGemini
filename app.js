7b959c6020995d14c42f1b9a21f3686a538f311f
InfoDenis <mensagemparavoce_54@hotmail.com>
2024-09-07 16:21:35 -0300

    Aula05 - Finalização do projeto com a pesquisa funcionando.

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
 // Função para realizar uma pesquisa e exibir os resultados na página.
 function pesquisar() {
   // Obtém a seção onde os resultados serão exibidos.
-  let section = document.getElementById("resultados-pesquisa");
+  let section       = document.getElementById("resultados-pesquisa");
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;  
+
+  // Retira espaços em branco do inicio e do fim da string.
+  campoPesquisa = campoPesquisa.trim();
+
+  // Se campoPesquisa for uma srting sem nada
+  if (campoPesquisa == "") {
+    section.innerHTML = "Não foi encontrado. Você precisa digitar o nome de um atleta ou esporte.";
+    return;
+  }
+  
+  campoPesquisa = campoPesquisa.toLowerCase();
+
   let resultados = ""; // String que armazenará os resultados formatados em HTML.
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
 
   // Itera sobre cada dado da pesquisa e constrói o HTML para cada resultado.
   for (let dado of dados) {
-    resultados += `
+    // Se titulo includes campoPesquisa
+    titulo = dado.titulo.toLowerCase();
+    descricao = dado.descricao.toLowerCase();
+    tags = dado.tags.toLocaleLowerCase();
+
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+      // Cria novo elemento
+      resultados += `
       <div class="item-resultado">
         <h2> <a href="#" target="_blank">${dado.titulo}</a></h2>
         <p class="descricao-meta">${dado.descricao}</p>
         <a href="${dado.link}" target="_blank">Mais Informações</a>
       </div>
     `;
+    }
   }
 
+  if (!resultados) {
+    resultados = "Nada foi encontrado.";
+  }
   // Atribui o HTML gerado à seção de resultados.
   section.innerHTML = resultados;
 }
--- dados.js
+++ dados.js
@@ -2,16 +2,19 @@ let dados = [
   {
     titulo: "Rayssa Leal",
     descricao: "Rayssa Leal, conhecida como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela se tornou a mais jovem medalhista olímpica da história do Brasil. Rayssa é um símbolo de superação e inspiração para jovens skatistas e atletas de diversas modalidades.",
-    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags: "skate sk8 fada"
   },
   {
     titulo: "Beatriz Souza",
     descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nas Olimpíadas de Paris. Sua trajetória é marcada por superação e dedicação, e ela se tornou um símbolo de representatividade para a comunidade negra no esporte.",
-    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    tags: "judô judo judoka judoca"
   },
   {
     titulo: "Rebeca Andrade",
     descricao: "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros com sua habilidade, determinação e carisma. Bicampeã olímpica, ela é considerada a maior medalhista da história do Brasil nos Jogos Olímpicos, com diversas medalhas em seu currículo. Rebeca se destaca por sua versatilidade, executando com maestria todos os aparelhos da ginástica artística. Além de seus feitos esportivos, ela é um exemplo de superação e inspira jovens atletas em todo o país.",
-    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags: "ginástica ginastica ouro"
   }
 ];
